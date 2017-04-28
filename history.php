fb9dd5aea6d72aeb2abf66ab1b7896f32464d052
Curtis Gibby <cgibby@gmail.com>
2017-04-28 08:18:22 -0600

    Initial commit;

---
+++ history.php
@@ -0,0 +1,93 @@
+<?php
+date_default_timezone_set('America/Denver');
+$startTime = strtotime('January 1');
+if (!empty($_GET['start_date'])) {
+	$startTime = strtotime($_GET['start_date']);
+}
+$startDate = date('j M Y', $startTime);
+$endTime = strtotime('11:59pm December 31');
+if (!empty($_GET['end_date'])) {
+	$endTime = strtotime($_GET['end_date']);
+}
+$endDate = date('j M Y', $endTime);
+$historyFile = 'history.json';
+$showsFile = 'shows.json';
+$events = json_decode(file_get_contents($historyFile), true);
+
+// echo("<P>events : "); echo "<pre>"; print_r($events); echo "</pre>"; // debug!
+// exit(); // debug!
+$shows = json_decode(file_get_contents($showsFile), true);
+$movieCount = $episodeCount = 0;
+$showTotals = array();
+foreach ($events['movies'] as $movieId => $movie) {
+	$watchTime = $movie[0];
+	if ($watchTime < $startTime || $watchTime > $endTime) {
+		continue;
+	}
+	$movieCount++;
+}
+foreach ($events['shows'] as $showId => $show) {
+	foreach ($show['e'] as $episode) {
+		// echo("<P>episode : "); echo "<pre>"; print_r($episode); echo "</pre>"; // debug!
+		$watchTime = $episode[0];
+		if ($watchTime < $startTime || $watchTime > $endTime) {
+			continue;
+		}
+		$episodeCount++;
+		if (!isset($showTotals[$showId])) {
+			$showTotals[$showId] = 0;
+		}
+		$showTotals[$showId]++;
+	}
+}
+arsort($showTotals);
+$eventCount = count($events);
+$pageTitle = "Trakt History - $startDate to $endDate";
+?>
+<html>
+	<head>
+		<title><?php echo $pageTitle; ?></title>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	</head>
+	<body>
+		
+		<?php
+		echo "<h1>$pageTitle</h1>";
+		echo("<P>Movie Count : " . $movieCount);
+		echo("<P>TV Episode Count : " . $episodeCount);
+		echo "<table class='table table-striped'><thead><tr>
+			<th>ID</th>
+			<th>Title</th>
+			<th>Image</th>
+			<th>Watch Count</th>
+		</tr></thead>";
+		foreach ($showTotals as $showId => $watchCount) {
+			$showImage = 'n/a';
+			$showTitle = 'Unknown';
+			if (array_key_exists($showId, $shows)) {
+				$showTitle = $shows[$showId]['title'];
+				$showTitle = '<a href=\'https://duckduckgo.com/?q=\\"' . urlencode($showTitle) . '"+"series+info"+site%3Athetvdb.com\'>' . $showTitle . '</a>';
+				if (!empty($shows[$showId]['image'])) {
+					$showImage = '<img src="https://images.weserv.nl/?url=thetvdb.com/banners/graphical/' . $shows[$showId]['image'] . '.jpg&h=50">';
+				}
+			}
+			echo "<tr>
+			<td><a href='http://trakt.tv/shows/" . $showId . "'>" . $showId . "</a></td>
+			<td>" . $showTitle . "</td>
+			<td>" . $showImage . "</td>
+			<td>" . $watchCount . "</td>
+		</tr>";
+		}
+		?>
+		</table>
+
+		<p><a href="https://trakt.tv/users/curtisgibby/history.json">Trakt History</a></p>
+		<ul class="list-inline">
+		<?php
+		foreach (range(2014, date('Y')) as $year) {
+			echo '<li><a href="/history.php?start_date=' . $year . '-01-01&end_date=' . ($year + 1) . '-01-01">' . $year . '</a></li>';
+		}
+		?>
+		</ul>
+	</body>
+</html>
