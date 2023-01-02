a670f8db96320d22c3e86c72317e0956c7ef3838
Curtis Gibby <cgibby@gmail.com>
2023-01-01 23:55:39 -0700

    Add python parser

---
+++ history.py
@@ -0,0 +1,51 @@
+import json
+from datetime import datetime
+
+with open('history.json') as f:
+    events = json.load(f)
+with open('shows.json') as f:
+    shows = json.load(f)
+
+startDateString = "2022-01-01T00:00:00"
+startDate = datetime.strptime(startDateString, '%Y-%m-%dT%H:%M:%S')
+
+endDateString = "2023-01-01T00:00:00"
+endDate = datetime.strptime(endDateString, '%Y-%m-%dT%H:%M:%S')
+
+episodeCount = movieCount = 0
+viewsByShow = {}
+
+for movieId in events['movies']:
+    movieView = events['movies'][movieId]
+    movieViewDatetime = datetime.fromtimestamp(movieView[0])
+    if startDate <= movieViewDatetime <= endDate:
+        movieCount += 1
+
+for showId in events['shows']:
+    showViews = events['shows'][showId]
+    show = shows.get(showId)
+    for episodeId in showViews['e']:
+        episodeViews = showViews['e'][episodeId]
+        episodeViewDatetime = datetime.fromtimestamp(episodeViews[0])
+        if startDate <= episodeViewDatetime <= endDate:
+            if showId in viewsByShow:
+                viewsByShow[showId] += 1
+            else:
+                viewsByShow[showId] = 1
+            episodeCount += 1
+            # if show:
+            #     print 'show[title]: {}'.format(show['title'])
+            # else:
+            #     print 'unknown show with ID of {}'.format(showId)
+            # print 'showId: {}'.format(showId)
+
+print 'movieCount: {}'.format(movieCount)
+print 'episodeCount: {}'.format(episodeCount)
+
+sortedViewsByShow = sorted(viewsByShow.items(), key=lambda x:x[1], reverse=True)
+rank = 0
+print("{: >5} {: <35} {: >5}".format("Rank", "Show Title", "View Count"))
+print("{: >5} {: <35} {: >5}".format("----", "----------", "----------"))
+for showId, showViewCount in sortedViewsByShow:
+    rank += 1
+    print("{: >5} {: <35} {: >5}".format(rank, shows[showId]['title'], showViewCount))
